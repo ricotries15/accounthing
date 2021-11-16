dca5f7243b997b86c6259b3e325e7e3601efdd9b
Anthony Benitez <anthony.bebor@gmail.com>
2021-11-16 18:43:08 -0500

    Minor changes to lib/error.bash

--- lib/error.bash
+++ lib/error.bash
@@ -15,8 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Error handling module
-
+### Error handling module
 
 error() {
    echo "Error: $1" >&2
